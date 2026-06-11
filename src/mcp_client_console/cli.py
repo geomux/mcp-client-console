7481264867acd52af8a7d65c2132251b787e93c4
geomux <Henry.LawrenceGEO@gmail.com>
2026-06-11 08:27:41 -0700

    feat: write main cli.py script with split main logic between sync'd and async'd logic.

--- src/mcp_client_console/cli.py
+++ src/mcp_client_console/cli.py
@@ -1,1 +1,38 @@
-# Main Python script to run the MCP Client Console
+# cli.py
+# User interface, load config, connect to active server, get tools via MCP with LLM
+
+import asyncio
+from mcp_client_console.config_loader import config_load
+from mcp_client_console.config_loader import get_active_server
+from mcp_client_console.client import open_session
+from mcp_client_console.client import get_tools
+from mcp_client_console.client import run_tool
+
+### Async'd logic | holds a session with the server and remote tool handling
+async def async_main(server: dict):
+    async with open_session(server["url"]) as session:
+        tools = await get_tools(session)
+        print("_" * 50)
+        print(f"\nConnected: {server['name']} @ {server['url']}")
+        print(f"\nAvailable Tools:")
+        for i, description in tools:
+            print(f" Name: {i}\nDescription:{description}")
+        print("_" * 50)
+        ### Section below to be replaced with LLM connections
+        ### Section below temporary hardcoded to run a test get_time() tool via MCP
+        result = await run_tool(session, "get_time", {})
+        print(f"\nget_time MCP tool says: {result}")
+
+### Sync'd logic | identifies config dictionary, gets the active server, runs async_main() to hold session with server
+def main():
+    config_file = config_load()
+    server = get_active_server(config_file)
+    asyncio.run(async_main(server))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
