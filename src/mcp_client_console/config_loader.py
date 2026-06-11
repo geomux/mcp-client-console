b4e74e20d91d727bbbc100c60fcfe8d384c0d21f
geomux <Henry.LawrenceGEO@gmail.com>
2026-06-11 06:47:26 -0700

    chore: cleanup terminal prints and add dev notes for clarity to future self

--- src/mcp_client_console/config_loader.py
+++ src/mcp_client_console/config_loader.py
@@ -1,5 +1,5 @@
 # config_loader.py
-# Finds, creates, and loads the user config.toml file
+# Finds, creates, and loads the user config.toml file for remote server access
 
 import sys
 import tomllib
@@ -9,11 +9,13 @@ from platformdirs import user_config_dir
 
 APP_NAME = "mcp-client-console"
 
+### Defines Config File destination filepath
 def config_path() -> Path:
     folder = Path(user_config_dir(APP_NAME, appauthor=False)) # appauthor=False stops Windows from adding an extra folder.
     file = folder / "config.toml"
     return file
 
+### Creates Config File if does not exist
 def config_create() -> Path:
     config_file = config_path()
     if not config_file.exists():
@@ -27,31 +29,37 @@ def config_create() -> Path:
         sys.exit(1)
     return config_file
 
-def config_load() -> dict: # returns dictionary because tomllib.load() converts config.toml into Python dictionary
+### Load Config File settings into Python dictionary
+def config_load() -> dict:
     config_file = config_create()
     with open(config_file, "rb") as f: # note that tomllib needs to read in binary, hence "rb"
-        config = tomllib.load(f)
-    return config
+        config_dictionary = tomllib.load(f)
+    return config_dictionary
 
-def get_active_server(config: dict) -> dict:
-    servers = config["server"]
-    if len(servers) == 1:
-        active_server = servers[0]
+### User chooses an active server from the Config File
+def get_active_server(config_dictionary: dict) -> dict:
+    available_servers = config_dictionary["server"]
+    if len(available_servers) == 1:
+        active_server = available_servers[0]
         return active_server
-    print(f"Servers in config: ")
-    for i, server in enumerate(servers, start=1):
-        print(f"{i}. {server['name']} @ {server['url']}")
-    active_server = input(f"Connect to a server. Enter number now: ")
-    active_server = active_server.strip()
-    server_number = int(active_server) - 1 # because Python indexes start at 0
-    if server_number < 0 or server_number >= len(servers):
-        print("Enter a listed server number.")
-        sys.exit(1)
-    return servers[server_number]
+    else:
+        print("_" * 50)
+        print(f"\nMultiple Servers Available: ")
+        for i, server in enumerate(available_servers, start=1):
+            print(f"{i}. {server['name']} @ {server['url']}")
+        print("_" * 50)
+        active_server = input(f"\nConnect to a server. Enter number now: ")
+        active_server = active_server.strip()
+        server_number = int(active_server) - 1 # because Python indexes start at 0
+        if server_number < 0 or server_number >= len(servers):
+            print("Enter a listed server number.")
+            sys.exit(1)
+        return servers[server_number]
 
 if __name__ == "__main__":
     config = config_load()
     server = get_active_server(config)
+    print("_" * 50)
     print(f"Server Config Loaded.")
     print(f"Active Server: {server['name']}, " @ ", {server['url']}")
 
