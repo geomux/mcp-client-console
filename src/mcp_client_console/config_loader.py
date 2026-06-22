175503ca4d4954ccd483df9000b61d0d13a7c9b6
geomux <Henry.LawrenceGEO@gmail.com>
2026-06-22 12:22:17 -0700

    feat: update get_active_server() function so it doesn't quit if enter wrong server number refactor: variable name changes to be clearer

--- src/mcp_client_console/config_loader.py
+++ src/mcp_client_console/config_loader.py
@@ -9,20 +9,22 @@ from platformdirs import user_config_dir
 
 APP_NAME = "mcp-client-console"
 
-### Defines destination filepath for Config File
 def config_path() -> Path:
+    """Defines destination filepath for Config File"""
     folder = Path(user_config_dir(APP_NAME, appauthor=False)) # appauthor=False stops Windows from adding an extra folder.
     file = folder / "config.toml"
     return file
 
-### Creates Config File (from template) if does not exist
 def config_create() -> Path:
+    """Creates Config File (from template) if does not exist"""
     config_file = config_path()
     if not config_file.exists():
         config_file.parent.mkdir(parents=True, exist_ok=True)
         template = files("mcp_client_console").joinpath("config_default.toml")
-        text = template.read_text()
-        config_file.write_text(text)
+        text = template.read_text(encoding="utf-8-sig")
+        config_file.write_text(text, encoding="utf-8-sig")
+            # this (encoding="utf-8-sig") argument to strip BOM (byte order mark) from config file if present.
+            # some programs save a BOM on the top of the file. BOM can be unexpected and cause crashes.
         config_file.chmod(0o600) # owner access only, security concerns as token lives in this file
         print("_"*50)
         print("\n[ CONFIG CREATED ]")
@@ -32,33 +34,39 @@ def config_create() -> Path:
         sys.exit(1)
     return config_file
 
-### Load settings from Config File into Python dictionary
 def config_load() -> dict:
+    """Load settings from Config File into Python dictionary"""
     config_file = config_create()
     config_text = config_file.read_text(encoding="utf-8-sig")
     config_dictionary = tomllib.loads(config_text)
     return config_dictionary
 
-### User chooses an active server from Config File
+
 def get_active_server(config_dictionary: dict) -> dict:
+    """User chooses an active server from Config File"""
     available_servers = config_dictionary["server"]
     if len(available_servers) == 1:
-        active_server = available_servers[0]
-        return active_server
-    else:
+        server_choice = available_servers[0]
+        return server_choice
+
+    still_choosing = True
+    while still_choosing == True:
         print("_" * 50)
         print(f"\n[ AVAILABLE SERVERS ]\n")
         for i, server in enumerate(available_servers, start=1):
             print(f"[ {i} ] {server['name']} @ {server['url']}")
         print("_" * 50)
         print(f"\nTo connect to a server, enter number below... ")
-        active_server = input(f">: ")
+        server_choice = input(f">: ").strip()
         print("_" * 50)
-        active_server = active_server.strip()
-        server_number = int(active_server) - 1 # because Python indexes start at 0
+        if server_choice.isdigit() == False:
+            print(f"\nEnter a server number listed above.")
+            continue
+
+        server_number = int(server_choice) - 1 # because Python indexes start at 0
         if server_number < 0 or server_number >= len(available_servers):
-            print("Enter a listed server number.")
-            sys.exit(1)
+            print(f"\nEnter a server number listed above.")
+            continue
         return available_servers[server_number]
 
 if __name__ == "__main__":
