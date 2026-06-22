77e5ae1a9f14636961a2a81be84fcc34ada694f3
geomux <Henry.LawrenceGEO@gmail.com>
2026-06-22 11:54:29 -0700

    feat: UX improvements, added input based program exits and retry loop after error handling.

--- src/mcp_client_console/cli.py
+++ src/mcp_client_console/cli.py
@@ -15,29 +15,46 @@ async def async_main(server: dict):
     async with open_session(server["url"]) as session:
         tools = await get_tools(session)
         print("_" * 50)
-        print(f"\nConnected: {server['name']} @ {server['url']}")
-        print(f"\nAvailable Tools:")
-        for name, description, _ in tools: # "_" here is for the currently unused inputSchema attribute
-            print(f" Name: {name}\nDescription:{description}")
+        print(f"\n[ CONNECTED ]")
+        print(f"\n{server['name']} @ {server['url']}\n")
         print("_" * 50)
+        print(f"\n[ AVAILABLE TOOLS]")
+        for name, description, _ in tools: # "_" here is for the currently unused inputSchema attribute
+            print(f"\nName: {name}\nDescription:{description}")
+        print("_" * 50)
+        print(f"\nEnter 'quit' or 'exit' to disconnect from server")
+        connection_status = True
+        while connection_status == True:
+            user_input = input(">: ").strip()
+            if user_input.lower() == "quit" or user_input.lower() == "exit":
+                print(f"Disconnecting from {server['name']}...")
+                connection_status = False
+            if not user_input:
+                continue
+
         ### ---
         ### Section below to be replaced with LLM connections
         ### Section below temporary hardcoded to run a test get_time() tool via MCP
         ### ---
         result = await run_tool(session, "get_time", {})
-        print(f"\nget_time MCP tool says: {result}")
+        print(f"\n[ NO LLM YET ] result of get_time() is: {result}")
 
 ### Sync'd logic | identifies config dictionary, gets the active server, runs async_main() to hold session with server
 def main():
     config_file = config_load()
-    server = get_active_server(config_file)
-    try:
-        asyncio.run(async_main(server))
-    except* httpx.ConnectError: # error handling (unique situation here since its for async process)
-        print("_" * 50)
-        print(f"\nERROR: Could not reach {server['name']} at {server['url']}.")
-        print("\nIs the server running?\n")
-        print("_" * 50)
+    connection_status = True
+    while connection_status == True:
+        server = get_active_server(config_file)
+        try:
+            print("...session begin...hello...")
+            asyncio.run(async_main(server))
+            print("...session over...goodbye...")
+            connection_status = False
+        except* httpx.ConnectError: # error handling (unique situation here since its for async process)
+            print("_" * 50)
+            print(f"\nERROR: Could not reach {server['name']} at {server['url']}.")
+            print("\nIs the server running?\n")
+            print("_" * 50)
 
 
 if __name__ == "__main__":
